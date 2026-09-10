3129bcb3ae01620d71a65c2f39bab783dcb65a17
Sarvothaman R J <sarvothamanmsd@gmail.com>
2026-09-10 18:54:53 +0530

    Create Main.java

---
+++ src/main/java/com/ambulance/Main.java
@@ -0,0 +1,11 @@
+package com.ambulance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Emergency Ambulance Dispatch System");
+        System.out.println("Application Started Successfully");
+
+    }
+}
